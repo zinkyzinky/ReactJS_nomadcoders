43069d225fe0fc7eb8e81616f9d120ae98c478bc
Jinhee Ahn <jhahn@blockodyssey.io>
2019-05-04 18:24:30 +0900

    List with .maps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const movies = [
+  {
+    title : "Up",
+    poster : "https://static.rogerebert.com/uploads/review/primary_image/reviews/up-2009/hero_Up-2009.jpg"
+  },
+  {
+    title : "Believer",
+    poster : "https://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/liveboard/fanzeel/f04f521eefdc418098eb12434ce96ea7.JPG"
+  },
+  {
+    title : "Star Wars",
+    poster : "https://file.mk.co.kr/meet/neds/2015/12/image_readtop_2015_1193248_14504095132280710.jpg"
+  },
+  {
+    title : "The Avengers",
+    poster : "https://www.enewstoday.co.kr/news/photo/201904/1291309_368979_5341.jpg"
+  },
+
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          {movies.map(movie => {
+            return <Movie title={movie.title} poster={movie.poster} />
+          })}
+
+          
+          {/* 위의 map 과 동일한 소스? 
+          {[
+            <Movie title={movies[0].title} poster={movies[0].poster} />
+            <Movie title={movies[1].title} poster={movies[1].poster} />
+            <Movie title={movies[2].title} poster={movies[2].poster} />
+            <Movie title={movies[3].title} poster={movies[3].poster} />
+          ]} */}
+      </div>
+    );
+  }
 }
 
 export default App;
