36f1c997072cd2c2988514dbde5ac7d74c21c8cc
zinkyzinky <lovefool08@naver.com>
2019-05-05 12:03:06 +0900

    Smart vs Dumb Component

--- src/Movie.js
+++ src/Movie.js
@@ -1,37 +1,72 @@
-import React, {Component} from 'react';
+import React from 'react'; //  {Component} 이건 이제 없어도 되니까 제거!!
 import './Movie.css';
 import PropTypes from 'prop-types';
 
-class Movie extends Component{
+// class Movie extends Component{
 
-    /** 부모에게 받은 컴포넌트 정보 체크 가능!!
-     *  타입, 필수 체크등 
-     */
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string
-    }
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        )
+//     /** 부모에게 받은 컴포넌트 정보 체크 가능!!
+//      *  타입, 필수 체크등 
+//      */
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster} />
+//                 <h1>{this.props.title}</h1>
+//             </div>
+//         )
         
-    }
+//     }
+// }
+
+/** functional component 만들어보기 
+ *  class 들은 그 안에 this 라는 키워드가 존재하므로, this.blahblah.... 가능하지만,
+ *  functional component는 클래스형이 아니라서.. this.~~ 접근을 할 수가 없다. 이미 props 를 쓰니까!
+*/
+function Movie({title, poster}){
+    return (
+        <div>
+            {/** this.props.poster 이라고 하면 에러!! 클래스가 아니니까!! */ }
+            <MoviePoster poster={poster} />  
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component {
+
+// 클래스형 컴포넌트 !
+// class MoviePoster extends Component {
     
-    static propTypes = {
-        poster : PropTypes.string.isRequired
-    }
-    render() {
-        return (
-            <img src={this.props.poster} alt=""/>
-        )
-    }
+//     static propTypes = {
+//         poster : PropTypes.string.isRequired
+//     }
+//     render() {
+//         return (
+//             <img src={this.props.poster} alt="Movie Poster"/>
+//         )
+//     }
+// }
+
+
+// functional component (state 가 없고, props 만 존재하며, 그냥 return 을 하기 위한 존재일뿐일 때!!!)
+// 그냥 1개의 props 만 있으면 됨. 1개!!! 1개의 props, 1개의 html 태그만 필요.
+// function render 없고, 라이프 사이클 또한 없음. 그저 return 만 있음!
+function MoviePoster({poster}){
+    return (
+        <img src={poster} alt="Movie Poster" />
+    )
+}
+
+// 유효성? 규격 검사
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
+}
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie
